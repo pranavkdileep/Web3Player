fac688a1716581c7512a4673e0de78b3a4cb2110
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2024-03-29 14:40:07 +0000

    Refactor page component and import dependencies

--- web3player-backend/app/watch/page.tsx
+++ web3player-backend/app/watch/page.tsx
@@ -1,68 +1,13 @@
-"use client";
+import React from 'react'
+import { Suspense } from 'react'
+import Page from './Home'
 
-import  {Relay} from 'nostr-tools/relay'
-import { useState,useEffect,Suspense } from 'react'
-import { useSearchParams } from 'next/navigation'
-
-
-function Page() {
-  const searchParams = useSearchParams()
-  const id = searchParams.get('id')
-  console.log(id)
-    const connectrealy = async () => {
-        const relay = await Relay.connect('wss://relay.damus.io')
-        if (relay.connected) {
-            console.log('connected')
-            return relay;
-        }
-    }
-    const [relay, setRelay] = useState(connectrealy());
-    const [content, setContent] = useState('');
-    const [contentJson, setContentJson] = useState({} as any);
-    
-    const get = async (id:string) => {
-      const relayInstance = await relay;
-      if (relayInstance) {
-        const sub = relayInstance.subscribe([
-          {
-            ids: [id],
-          },
-        ], {
-          onevent(event) {
-            console.log('we got the event we wanted:', event.content);
-            setContent(event.content)
-          },
-          oneose() {
-            sub.close()
-          }
-        })
-      }
-    }
-    useEffect(() => {
-      if (id) {
-        get(id);
-      }
-    }, [id])
-
-    useEffect(() => {
-      if (content) {
-        setContentJson(JSON.parse(content))
-      }
-    }, [content])
-
-    
-
-    
-       
+function page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}> 
-    <div> 
-      <p>Title : {contentJson.title}</p>
-      <p>Descrption  : {contentJson.description} </p>
-      <p>Video  :  {contentJson.video}</p>
-      </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page />
     </Suspense>
   )
 }
 
-export default Page
+export default page
